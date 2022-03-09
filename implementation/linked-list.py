355a519efd017f23afc190b23045a0d9537600ad
kwangjong <choikj0903@gmail.com>
2022-03-09 16:06:39 +0900

    update linked-list.py

--- implementation/linked-list.py
+++ implementation/linked-list.py
@@ -7,42 +7,39 @@ class LinkedList:
   def __init__(self):
     self.__head = None
     self.__tail = None
-    self.__size = 0
 
   def append(self, val): #O(1)
-    if self.__size == 0:
+    if self.__head == None:
       self.__head = Node(val)
       self.__tail = self.__head
    
     else:
       self.__tail.next = Node(val)
       self.__tail = self.__tail.next
-      
-    self.__size += 1
 
   def get(self, index): #O(n)
-    if index >= self.__size:
+    if self.__head == None:
       return None
 
     curr = self.__head
     while index > 0:
+      if curr.next == None:
+        return None
       curr = curr.next
       index -= 1
 
     return curr.val
 
   def delete(self, index): #O(n)
-    if index >= self.__size:
-      return True
+    if self.__head == None:
+      return False
 
     curr = self.__head
     while index > 1:
+      if curr.next == None:
+        return False
       curr = curr.next
       index -= 1
     curr.next = curr.next.next
-    self.__size -= 1
-
-    return False
-
-  def size(self):
-    return self.__size
+    
+    return True
