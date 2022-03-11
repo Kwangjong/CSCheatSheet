dff84407dc398a26d8940880e9f39a2cd69fc406
kwangjong <choikj0903@gmail.com>
2022-03-11 20:09:24 +0900

    update selection-sort.py

--- implementation/selection-sort.py
+++ implementation/selection-sort.py
@@ -3,9 +3,9 @@ def selectionSort(unsorted):
 	for i in range(len(array)):
 		smallestInd = i
 		for j in range(i+1, len(array)): # find index of the smallest elements started from i+1
-			if(array[j] < array[smallestInd]):
+			if array[j] < array[smallestInd]:
 				smallestInd = j
-				
+
 		array[i], array[smallestInd] = array[smallestInd], array[i] # swap i and smallestInd
 
 	return array
